4aeadcb8bce6de3c9381996fa1d4a5697797e7e3
Camila Bazzo <cdbazzo@hotmail.com>
2021-08-10 00:28:50 -0300

    Adicionado ConexaoMySQL

---
+++ src/dao/ConexaoMySQL.java
@@ -0,0 +1,77 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexaoMySQL {
+	private static Connection con;
+	private static String status;
+
+	public ConexaoMySQL() {
+
+	}
+
+	public static void abrirConexao() {
+		// Carregando o JDBC Driver padrão
+		String driverName = "com.mysql.cj.jdbc.Driver";
+		// Faz o registro do driver jdbc
+		try {
+			Class.forName(driverName);
+		} catch (ClassNotFoundException e1) {
+			 e1.printStackTrace();
+			 System.out.println("O driver especificado nao foi encontrado.");
+		}
+		// Configurando a nossa conexão com um banco de dados
+		String serverName = "localhost";
+		String database = "tce";
+		String url = "jdbc:mysql://localhost:3306/tce";
+		String userName = "poo";
+		String password = "senha";
+
+		try {
+			con = DriverManager.getConnection(url, userName, password);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("Nao foi possivel conectar ao Banco de Dados.");
+		}
+
+		if (con != null) {
+			status = "STATUS -----> Conectado com sucesso!";
+		} else {
+			status = "STATUS -----> Não foi possível realizar a conexão :(";
+		}
+	}
+
+	public static boolean fecharConexao() {
+		try {
+			con.close();
+			System.out.println("Conexao encerrada!");
+			return true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	public static void obterStatusConexao() {
+		System.out.println(status);
+	}
+
+	public static Connection getCon() {
+		return con;
+	}
+
+	public static void setCon(Connection con) {
+		ConexaoMySQL.con = con;
+	}
+
+	public static void main(String[] args) {
+		ConexaoMySQL.abrirConexao();
+		ConexaoMySQL.obterStatusConexao();
+		ConexaoMySQL.fecharConexao();
+	}
+
+}
